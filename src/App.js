b7552377a97f6834f99bb14d8180d4e4e58e2b99
Apple <apple@Sotanomakku.local>
2022-12-02 22:27:19 +0900

    コンポーネント

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+return (
+  <>
+    <h2>Hello World!</h2>
+    <p>How are you?</p>
+  </>
+  )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-return (<h1>今日は</h1>);
-};
+import App from "./App"
 
 ReactDom.render(<App />, document.getElementById("root"));
 
