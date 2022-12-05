acdf15e4f9001ab92b0c2270b0c65809c73e79d1
Apple <apple@Sotanomakku.local>
2022-12-05 21:41:24 +0900

    useStateとuseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
 return (
   <>
     <h2 style={{ color: "green" }}>Hello World!</h2>
     <ColorfulMessage ColorfulMessage color="green">俺はドンキーコング</ColorfulMessage>
     <ColorfulMessage ColorfulMessage color="pink">私はディクシーコング</ColorfulMessage>
-    <button onClick={onClickButton}>絶対押してね</button>
+    <button onClick={onClickCountUp}>絶対押してね</button>
+    <p>{ num }</p>
+    { faceShowFlag  && <p>♪( ´θ｀)ノ</p>}
   </>
   )
 };
